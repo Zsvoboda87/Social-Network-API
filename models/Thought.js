9436d52c10796fa4b087127346d6a244a6a8ded9
Zsvob <svobodazachary@gmail.com>
2022-03-24 20:43:10 -0400

    start thought schema

---
+++ models/Thought.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const ThoughtSchema = new Schema(
+    {
+
+    }
+);
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
