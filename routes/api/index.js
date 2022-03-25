2bdb597551e983e861fb43cb9de1a77361fa3311
Zsvob <svobodazachary@gmail.com>
2022-03-25 11:09:24 -0400

    user api routes written needs test) git add . ; q exit
    :q

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,35 +1,9 @@
 const router = require("express").Router();
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-const { User, Thought } = require("../../models");
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
-router.get("/", (req, res) => {
-  // Use the `find()` method to get all of the notes in the database's collection
-  User.find({})
-    .then((dbNote) => res.json(dbNote))
-    .catch((err) => res.status(400).json(err));
-  //
-});
-
-router.get("/:id", ({ params, body }, res) => {
-    Note.findOne({ _id: params.id }, body, { new: true,})
-    .then((dbNote) => {
-      if (!dbNote) {
-        res.status(404).json({ message: "No pizza found with this id!" });
-        return;
-      }
-      res.json(dbNote);
-    })
-    .catch((err) => res.status(400).json(err));
-});
-
-});
-
-router.post("/users", ({ body }, res) => {
-  // Use the `create()` method to create a new note using the data in `body`
-  User.create(body)
-    .then((dbNote) => res.json(dbNote))
-    .catch((err) => res.status(400).json(err));
-  //
-});
 
 module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { User, Thought } = require("../../models");
+
+// Get All Users
+router.get("/", (req, res) => {
+  User.find({})
+    .then((dbUser) => res.json(dbUser))
+    .catch((err) => res.status(400).json(err));
+});
+
+// Get User by ID
+router.get("/:id", ({ params }, res) => {
+  Note.findOne({ _id: params.id })
+    .populate({ path: "thoughts" })
+    .then((dbUser) => {
+      if (!dbUser) {
+        res.status(404).json({ message: "No user found with this ID!" });
+        return;
+      }
+      res.json(dbUser);
+    })
+    .catch((err) => res.status(400).json(err));
+});
+
+// Post New User
+router.post("/users", ({ body }, res) => {
+  // Use the `create()` method to create a new note using the data in `body`
+  User.create(body)
+    .then((dbUser) => res.json(dbUser))
+    .catch((err) => res.status(400).json(err));
+  //
+});
+
+router.post('/update/:id', ({ params, body }, res) => {
+  User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    .then(dbUser => {
+      if (!dbUser) {
+        res.json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete('/delete/:id', ({params}, res) => {
+  User.findOneAndDelete({_id: params.id})
+  .then(dbUser => {
+    if (!dbUser) {
+      res.json({message: "No user found with this id!"});
+      return;
+    }
+    res.json(dbUser);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
+
+
+module.exports = router;
